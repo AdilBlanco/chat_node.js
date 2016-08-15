928222e6c43e5cb40408ca91cbd9d5a88d93db7c
Blanco <adillabiad@gmail.com>
2016-08-15 08:46:06 -0400

    Done :)

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var mongodb = require('mongodb').MongoClient;
+/*var mongodb = require('mongodb').MongoClient;
 var client = require('socket.io').listen(8080).sockets;
 var url = 'mongodb://127.0.0.1/chat';
 
@@ -22,6 +22,55 @@ mongodb.connect(url, function(err, db) {
 
                 console.log('Inserted');
             });
+        });
+    });
+});*/
+
+var mongo = require('mongodb').MongoClient,
+    client = require('socket.io').listen(8080).sockets;
+
+mongo.connect('mongodb://127.0.0.1/chat', function(err, db) {
+    if (err) throw err;
+
+    client.on('connection', function(socket) {
+
+        var col = db.collection('messages'),
+            sendStatus = function(s) {
+                socket.emit('status', s);
+            };
+
+        col.find().limit(100).sort({
+            _id: 1
+        }).toArray(function(err, res) {
+            if (err) throw err;
+            socket.emit('output', res);
+        });
+
+        //wait for input
+        socket.on('input', function(data) {
+            var name = data.name;
+            var message = data.message;
+
+            whitespace = /^\s*$/;
+
+            if (whitespace.test(name) || whitespace.test(message)) {
+                sendStatus('Name and Message Required');
+            } else {
+                col.insert({
+                    name: name,
+                    message: message
+                }, function() {
+
+                    //emit latest messages to all clients
+                    client.emit('output', [data]);
+
+                    sendStatus({
+                        message: "Message sent",
+                        clear: true
+                    });
+                });
+            }
+
         });
     });
 });
